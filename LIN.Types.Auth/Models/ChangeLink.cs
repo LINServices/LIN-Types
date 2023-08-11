b33a11348bbebca2da0bb601c0a03d88a7b09a94
Alex <giraldojhong4@gmail.com>
2023-08-11 02:11:10 -0500

    Nuevos tipos Magic Link

---
+++ LIN.Types.Auth/Models/ChangeLink.cs
@@ -0,0 +1,17 @@
+﻿namespace LIN.Types.Auth.Models;
+
+
+public class UniqueLink
+{
+
+    public int ID { get; set; }
+
+    public string Key { get; set; } = string.Empty;
+
+    public int AccountID { get; set; }
+
+    public MagicLinkStatus Status { get; set; }
+
+    public DateTime Vencimiento { get; set; }
+
+}
---
+++ LIN.Types.Auth/Models/MailMagicLink.cs
@@ -0,0 +1,12 @@
+﻿namespace LIN.Types.Auth.Models;
+
+
+public class MailMagicLink
+{
+    public int ID { get; set; }
+    public string Key { get; set; } = string.Empty;
+    public int Email { get; set; }
+    public MagicLinkStatus Status { get; set; }
+    public DateTime Vencimiento { get; set; }
+
+}
