d00403c47ae632d686432f78c1be34e2ff1dabb0
Alex <giraldojhong4@gmail.com>
2023-08-13 23:55:38 -0500

    Nuevo Session Model

---
+++ LIN.Types.Auth/Abstracts/SessionModel.cs
@@ -0,0 +1,11 @@
+﻿namespace LIN.Types.Auth.Abstracts;
+
+
+public class SessionModel<T>  where T : new()
+{
+
+    public Models.AccountModel Account { get; set; } = new();
+
+    public T Profile { get; set; } = new();
+
+}
