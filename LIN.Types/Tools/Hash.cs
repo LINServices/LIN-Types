adc47b8ddcf476ea9f74bb90f23edf0253d8a4d0
Alex <giraldojhong4@gmail.com>
2023-08-19 17:23:57 -0500

    Hash validation

---
+++ LIN.Types/Tools/Hash.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.Cryptography;
+
+namespace LIN.Types.Tools;
+
+public class Hash
+{
+
+
+    /// <summary>
+    /// Genera el Hash de un perfil
+    /// </summary>
+    /// <param name="profile">Perfil</param>
+    public static string GenerateHash(IProfile profile)
+    {
+        string final = $"{profile.AccountID}|{profile.ID}";
+        var bytes = SHA512.HashData(Encoding.UTF8.GetBytes(final));
+        return Convert.ToBase64String(bytes);
+    }
+
+
+
+    /// <summary>
+    /// Valida el hash de un perfil
+    /// </summary>
+    /// <param name="profile">Perfil</param>
+    public static bool ValidateHash(IProfile profile)
+    {
+        var now = GenerateHash(profile);
+        return now == profile.Hash;
+    }
+
+
+}
