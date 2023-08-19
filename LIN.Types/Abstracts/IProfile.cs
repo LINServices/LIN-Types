ef957d8293f1f1eb3caa0406a7a089478267a82e
Alex <giraldojhong4@gmail.com>
2023-08-19 17:14:21 -0500

    Interfaz para perfiles de LIN

---
+++ LIN.Types/Abstracts/IProfile.cs
@@ -0,0 +1,27 @@
+﻿namespace LIN.Types.Auth.Abstracts;
+
+
+/// <summary>
+/// Recomendaciones de lo que debe de tener un Perfil en LIN
+/// </summary>
+public interface IProfile
+{
+
+    /// <summary>
+    /// ID del perfil
+    /// </summary>
+    public int ID { get; set; }
+
+
+    /// <summary>
+    /// ID de la cuenta en LIN Auth
+    /// </summary>
+    public int AccountID { get; set; }
+
+
+    /// <summary>
+    /// Hash de verificación de seguridad
+    /// </summary>
+    public string Hash { get; set; }
+
+}
