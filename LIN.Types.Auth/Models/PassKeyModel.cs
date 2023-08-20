faa40737dd4fb4c54a51e5835e516613e48b2809
Alex <giraldojhong4@gmail.com>
2023-08-19 23:00:01 -0500

    Cambios en Passkey model

--- LIN.Types.Auth/Models/PassKeyModel.cs
+++ LIN.Types.Auth/Models/PassKeyModel.cs
@@ -8,8 +8,6 @@ public class PassKeyModel
 
     public string User { get; set; } = string.Empty;
 
-    public int AccountID { get; set; }
-
     public string ApplicationName { get; set; } = string.Empty;
 
     public string Token { get; set; } = string.Empty;
