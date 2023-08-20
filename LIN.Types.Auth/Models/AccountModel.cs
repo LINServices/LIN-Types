0620ca6f4f8e345e346417da52cd7041c71d99ff
Alex <giraldojhong4@gmail.com>
2023-08-20 11:03:25 -0500

    Nuevo tipo para Organizaciones

--- LIN.Types.Auth/Models/AccountModel.cs
+++ LIN.Types.Auth/Models/AccountModel.cs
@@ -11,15 +11,13 @@ public class AccountModel
 
     [Column("CONTRASENA")]
     public string Contraseña { get; set; } = string.Empty;
-
     public Genders Genero { get; set; } = Genders.Undefined;
-
     [Column("CREACION")]
     public DateTime Creación { get; set; }
     public AccountStatus Estado { get; set; } = AccountStatus.Normal;
     public AccountRoles Rol { get; set; } = AccountRoles.User;
     public AccountVisibility Visibilidad { get; set; } = AccountVisibility.Visible;
     public AccountBadges Insignia { get; set; } = AccountBadges.None;
-
+    public OrganizationModel? Organization { get; set; } = null;
 
 }
---
+++ LIN.Types.Auth/Models/OrganizationModel.cs
@@ -0,0 +1,12 @@
+﻿namespace LIN.Types.Auth.Models;
+
+
+public class OrganizationModel
+{
+
+    public int ID { get; set; } = 0;
+    public string Name { get; set; } = string.Empty;
+    public string Domain { get; set; } = string.Empty;
+    public List<AccountModel> Members { get; set; } = new();
+
+}
