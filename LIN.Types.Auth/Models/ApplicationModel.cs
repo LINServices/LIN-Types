c8434a7e0c7785cf2d7b14a523a5c4764450118d
Alex <giraldojhong4@gmail.com>
2023-08-20 11:06:11 -0500

    Nuevo tipo Application

---
+++ LIN.Types.Auth/Models/ApplicationModel.cs
@@ -0,0 +1,11 @@
+﻿namespace LIN.Types.Auth.Models;
+
+
+public class ApplicationModel
+{
+
+    public int ID { get; set; } = 0;
+    public string Name { get; set; } = string.Empty;
+    public string Key { get; set; } = string.Empty;
+
+}
