628d96df9f836eb632943d9a86669ad9a6e9a0cf
Alex <giraldojhong4@gmail.com>
2023-08-20 23:43:35 -0500

    Cambios en las relaciones

--- LIN.Types.Auth/Models/OrganizationAccessModel.cs
+++ LIN.Types.Auth/Models/OrganizationAccessModel.cs
@@ -5,7 +5,9 @@ public class OrganizationAccessModel
 {
 
     public int ID { get; set; } = 0;
+
     public virtual AccountModel Member { get; set; }
+
     public virtual OrganizationModel Organization { get; set; }
 
 }
