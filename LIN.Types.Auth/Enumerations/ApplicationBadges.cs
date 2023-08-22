a6a6e0f4d1b744564e73da85ebf010c048c2773f
Alex <giraldojhong4@gmail.com>
2023-08-22 13:54:44 -0500

    Applications Badgets

---
+++ LIN.Types.Auth/Enumerations/ApplicationBadges.cs
@@ -0,0 +1,20 @@
+﻿namespace LIN.Types.Auth.Enumerations;
+
+
+/// <summary>
+/// Insignias que puede tener una APP
+/// </summary>
+public enum ApplicationBadges
+{
+
+    /// <summary>
+    /// Sin insignia
+    /// </summary>
+    None,
+
+    /// <summary>
+    /// Insignia verificado
+    /// </summary>
+    Verified,
+
+}
--- LIN.Types.Auth/Models/ApplicationModel.cs
+++ LIN.Types.Auth/Models/ApplicationModel.cs
@@ -7,6 +7,8 @@ public class ApplicationModel
     public int ID { get; set; } = 0;
     public string Name { get; set; } = string.Empty;
     public string Key { get; set; } = string.Empty;
+    public ApplicationBadges Badge { get; set; } = ApplicationBadges.None;
     public int AccountID { get; set; }
+
 
 }
