f681d817675e9d2cccf7b8121e8dc88829185ff8
Alex <giraldojhong4@gmail.com>
2023-08-22 13:58:11 -0500

    Application Uid

--- LIN.Types.Auth/Models/ApplicationModel.cs
+++ LIN.Types.Auth/Models/ApplicationModel.cs
@@ -6,6 +6,7 @@ public class ApplicationModel
 
     public int ID { get; set; } = 0;
     public string Name { get; set; } = string.Empty;
+    public string ApplicationUid { get; set; } = string.Empty;
     public string Key { get; set; } = string.Empty;
     public ApplicationBadges Badge { get; set; } = ApplicationBadges.None;
     public int AccountID { get; set; }
