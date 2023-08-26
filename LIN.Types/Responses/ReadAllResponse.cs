d6a8ed40235c17ae25322bf5f70891e8152c9d91
Alex <giraldojhong4@gmail.com>
2023-08-26 10:35:37 -0500

    Correccion

--- LIN.Types/Responses/ReadAllResponse.cs
+++ LIN.Types/Responses/ReadAllResponse.cs
@@ -1,45 +1,44 @@
-﻿namespace LIN.Types.Responses
+﻿namespace LIN.Types.Responses;
+
+
+public class ReadAllResponse<M> : ResponseBase
 {
-    public class ReadAllResponse<M> : ResponseBase
+
+    /// <summary>
+    /// Lista de modelos
+    /// </summary>
+    public List<M> Models { get; set; }
+
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    public ReadAllResponse()
     {
-
-        /// <summary>
-        /// Lista de modelos
-        /// </summary>
-        public List<M> Models { get; set; }
-
-
-        /// <summary>
-        /// Constructor
-        /// </summary>
-        public ReadAllResponse()
-        {
-            Response = Responses.Undefined;
-            Models = new();
-        }
-
-
-
-        /// <summary>
-        /// Constructor
-        /// </summary>
-        public ReadAllResponse(Responses response = Responses.Undefined)
-        {
-            Response = response;
-            Models = new();
-        }
-
-
-        /// <summary>
-        /// Constructor
-        /// </summary>
-        public ReadAllResponse(Responses response, List<M> models)
-        {
-            Response = response;
-            Models = models ?? new();
-        }
-
-
-
+        Response = Responses.Undefined;
+        Models = new();
     }
+
+
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    public ReadAllResponse(Responses response = Responses.Undefined)
+    {
+        Response = response;
+        Models = new();
+    }
+
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    public ReadAllResponse(Responses response, List<M> models)
+    {
+        Response = response;
+        Models = models ?? new();
+    }
+
+
 }
