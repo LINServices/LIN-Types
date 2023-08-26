233543cc249f818bb12a8b9e0bfbd52a18299571
Alex <giraldojhong4@gmail.com>
2023-08-26 15:01:11 -0500

    Correccion de tipos en LIN Communication

--- LIN.Types.Communication/Models/ProfileModel.cs
+++ LIN.Types.Communication/Models/ProfileModel.cs
@@ -5,6 +5,9 @@ public class ProfileModel
 {
     public int ID { get; set; }
     public string Alias { get; set; } = string.Empty;
+
+    public int AccountID { get; set; }
+
     public List<ProfileModel> Friends { get; set; } = new();
 
 }
