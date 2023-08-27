335be38c275440afd2303d5ad14b9a656eb8eb52
Alex <giraldojhong4@gmail.com>
2023-08-26 22:02:06 -0500

    Correccion de ConversacionModel

--- LIN.Types.Communication/Models/ConversacionModel.cs
+++ LIN.Types.Communication/Models/ConversacionModel.cs
@@ -6,6 +6,7 @@ namespace LIN.Types.Communication.Models;
 public class ConversationModel
 {
 
+    public int ID { get; set; }
     public string Name { get; set; }
     public List<MemberChatModel> Members { get; set; }
     public List<MessageModel> Mensajes { get; set; }
