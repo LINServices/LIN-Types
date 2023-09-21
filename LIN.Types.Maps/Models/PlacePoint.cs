0a9d786c65a372c257ef1ab6b9ba9e1406469e59
Alex <giraldojhong4@gmail.com>
2023-09-21 10:51:37 -0500

    Time en points

--- LIN.Types.Maps/Models/PlacePoint.cs
+++ LIN.Types.Maps/Models/PlacePoint.cs
@@ -6,6 +6,7 @@ internal class PlacePoint
     public int ID { get; set; }
     public double Longitude { get; set; }
     public double Latitude { get; set; }
+    public DateTime Time { get; set; }
     public ProfileModel Profile { get; set; }
 
 }
