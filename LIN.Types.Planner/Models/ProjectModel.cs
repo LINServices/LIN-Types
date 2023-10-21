92254b0bafb70fa327fd1b414c68c0dc4be2a86b
Alex <giraldojhong4@gmail.com>
2023-10-21 15:59:37 -0500

    Cambio de nombres de modelos

--- LIN.Types.Planner/Models/ProjectModel.cs
+++ LIN.Types.Planner/Models/ProjectModel.cs
@@ -7,7 +7,7 @@ public class ProjectModel
     public int Id { get; set; } 
     public string Name { get; set; }
     public bool IsClose { get; set; }
-    public List<WorkModel> Tasks { get; set; }
+    public List<ItemModel> Tasks { get; set; }
     public List<MemberModel> Members { get; set; }
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
