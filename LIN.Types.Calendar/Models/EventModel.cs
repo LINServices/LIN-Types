1c7298f98d812cd402b3981a1856e8ce74dd117a
Alex <giraldojhong4@gmail.com>
2023-11-16 11:53:26 -0500

    Tipos de LIN Calendar

---
+++ LIN.Types.Calendar/Models/EventModel.cs
@@ -0,0 +1,12 @@
+﻿namespace LIN.Types.Calendar.Models;
+
+
+public class EventModel
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    public DateTime DateStart { get; set; }
+    public DateTime EndStart { get; set; }
+    public ProfileModel Profile { get; set; }
+
+}
