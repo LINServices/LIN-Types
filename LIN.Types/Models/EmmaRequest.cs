6f63cc51bf14b2c517af4bb4ca49e75fb5d6bb1f
Alex <giraldojhong4@gmail.com>
2024-03-13 16:24:51 -0500

    Limpieza

--- LIN.Types/Models/EmmaRequest.cs
+++ LIN.Types/Models/EmmaRequest.cs
@@ -8,13 +8,11 @@ public class EmmaRequest
     public bool IncludeAppContext { get; set; } = true;
     public bool IncludeEmmaMethods { get; set; } = true;
     public bool IncludeGeneralMethods { get; set; } = true;
-    public bool IncludeAppsMethods { get; set; } = true;
     public bool IncludeDefaultTopics { get; set; } = true;
 
 
     public string AppContext {  get; set; }
     public string EmmaMethods {  get; set; }
-    public string AppsMethods {  get; set; }
     public string AdditionalTopics {  get; set; }
 
     public string Asks {  get; set; }
