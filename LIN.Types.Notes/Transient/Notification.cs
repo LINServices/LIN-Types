96391b9d3896d1febb73cf9a65a70ec97b12cabf
Alex <giraldojhong4@gmail.com>
2024-05-08 20:57:55 -0500

    Mejoras

--- LIN.Types.Notes/Transient/Notification.cs
+++ LIN.Types.Notes/Transient/Notification.cs
@@ -1,10 +1,12 @@
 ﻿namespace LIN.Types.Notes.Transient;
 
-public class Notificacion
+
+public class Notification
 {
+
     public int ID { get; set; } = 0;
-    public string Inventario { get; set; } = string.Empty;
-    public int InventarioID { get; set; } = 0;
+    public string NoteName { get; set; } = string.Empty;
+    public int NoteId { get; set; } = 0;
     public string UsuarioInvitador { get; set; } = string.Empty;
     public DateTime Fecha { get; set; }
 
