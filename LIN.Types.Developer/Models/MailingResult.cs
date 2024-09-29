157b4607d55d6f83b3ada94c26ad4e28859d3fd6
Alexander Giraldo <giraldojhong4@gmail.com>
2024-09-28 20:41:28 -0500

    Mejora de servicios

---
+++ LIN.Types.Developer/Models/MailingResult.cs
@@ -0,0 +1,10 @@
+﻿namespace LIN.Types.Developer.Models;
+
+public class MailingResult
+{
+    public int Id { get; set; }
+    public int StatusCode { get; set; }
+    public string EndMessage { get; set; } = string.Empty;
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+}
