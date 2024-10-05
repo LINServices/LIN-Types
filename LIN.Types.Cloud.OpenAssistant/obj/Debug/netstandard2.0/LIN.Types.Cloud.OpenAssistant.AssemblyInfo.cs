e7a361f008680cc4b6cbd8b4b89beaaaa4cac166
Alexander Giraldo <giraldojhong4@gmail.com>
2024-10-05 15:33:58 -0500

    Update

--- LIN.Types.Cloud.OpenAssistant/obj/Debug/netstandard2.0/LIN.Types.Cloud.OpenAssistant.AssemblyInfo.cs
+++ LIN.Types.Cloud.OpenAssistant/obj/Debug/netstandard2.0/LIN.Types.Cloud.OpenAssistant.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LIN.Types.Cloud.OpenAssistant")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+787b4755f95ec199d42bc5f93bd3e084b856e65f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c6f5a9f75f4ab7ef8eec0b5eab028422eccdfe0")]
 [assembly: System.Reflection.AssemblyProductAttribute("LIN.Types.Cloud.OpenAssistant")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LIN.Types.Cloud.OpenAssistant")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
