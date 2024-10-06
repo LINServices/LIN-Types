b75fb89e7e0b88074ac00287073ff22f4ebd3c4b
Alexander Giraldo <giraldojhong4@gmail.com>
2024-10-05 19:21:31 -0500

    Mejoras UI

--- LIN.Types.Cloud.OpenAssistant/obj/Debug/netstandard2.0/LIN.Types.Cloud.OpenAssistant.AssemblyInfo.cs
+++ LIN.Types.Cloud.OpenAssistant/obj/Debug/netstandard2.0/LIN.Types.Cloud.OpenAssistant.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LIN.Types.Cloud.OpenAssistant")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5adc68c675f9ef466a996b5f9856923bc94463b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b568cf317990e74ec097adbfbbde02b7b9e8cdb8")]
 [assembly: System.Reflection.AssemblyProductAttribute("LIN.Types.Cloud.OpenAssistant")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LIN.Types.Cloud.OpenAssistant")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
