4aceaba476d3ca7dbe8cde734408500d8db7c525
Alexander Giraldo <giraldojhong4@gmail.com>
2024-11-03 00:03:42 -0500

    Mejoras de UI

---
+++ LIN.Types.Mail/Models/ProfileModel.cs
@@ -0,0 +1,9 @@
+﻿namespace LIN.Types.Mail.Models;
+
+public class ProfileModel
+{
+    public string Email { get; set; }
+    public long MaxSize { get; set; }
+    public float Size { get; set; }
+    public bool IsActive {  get; set; }
+}
