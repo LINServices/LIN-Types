74bfac628464268dcbd7f6a4404ef346a193e689
Alexander Giraldo <giraldojhong4@gmail.com>
2024-11-24 11:16:01 -0500

    Mejoras

--- LIN.Types.Cloud/Models/BucketModel.cs
+++ LIN.Types.Cloud/Models/BucketModel.cs
@@ -4,6 +4,7 @@ public class BucketModel
 {
     public int Id { get; set; }
     public long MaxSize { get; set; }
+    public double ActualSize { get; set; }
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public int ProjectId { get; set; }
