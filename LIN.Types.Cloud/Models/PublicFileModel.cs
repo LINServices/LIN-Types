f2ec94467fe9136d19dafc408602b58e3ce66535
Alexander Giraldo <giraldojhong4@gmail.com>
2024-11-26 21:44:57 -0500

    Mejoras generales

---
+++ LIN.Types.Cloud/Models/PublicFileModel.cs
@@ -0,0 +1,9 @@
+﻿namespace LIN.Types.Cloud.Models;
+
+public class PublicFileModel
+{
+    public string Key { get; set; }
+    public string Path { get; set; }
+    public DateTime Expires { get; set; }
+    public int Bucket {  get; set; }
+}
