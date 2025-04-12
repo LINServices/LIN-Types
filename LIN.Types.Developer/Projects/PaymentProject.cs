2ff3fb7daf5e9930c06163f5460d9bfb6113475b
Alex Giraldo <giraldojhong4@gmail.com>
2025-04-12 12:24:22 -0500

    Mejoras

---
+++ LIN.Types.Developer/Projects/PaymentProject.cs
@@ -0,0 +1,10 @@
+﻿namespace LIN.Types.Developer.Projects;
+
+public class PaymentProject : Models.ProjectDataModel
+{
+    public string UserName { get; set; } = string.Empty;
+    public string UserMail { get; set; } = string.Empty;
+    public string UserLocation { get; set; } = string.Empty;
+    public bool UserAllowBilling { get; set; } = false;
+    public bool UserAllowBuy { get; set; } = false;
+}
