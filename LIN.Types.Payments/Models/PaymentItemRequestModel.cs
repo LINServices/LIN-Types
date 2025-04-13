13559e2e09922a9847d8d372ccc5eae50e53c8eb
Alex Giraldo <giraldojhong4@gmail.com>
2025-04-13 12:20:42 -0500

    Mejoras

---
+++ LIN.Types.Payments/Models/PaymentItemRequestModel.cs
@@ -0,0 +1,10 @@
+﻿namespace LIN.Types.Payments.Models;
+
+public class PaymentItemRequestModel
+{
+    public int Id { get; set; }
+    public int Quantity { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Picture { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+}
