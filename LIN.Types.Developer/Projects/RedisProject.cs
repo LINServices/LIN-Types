5b5bd5f98e39ee10f42f4dc5a44943361e690470
Alex Giraldo <giraldojhong4@gmail.com>
2025-04-15 20:22:31 -0500

    mejoras

--- LIN.Types.Developer/Projects/RedisProject.cs
+++ LIN.Types.Developer/Projects/RedisProject.cs
@@ -1,8 +1,9 @@
 ﻿namespace LIN.Types.Developer.Projects;
 
-public class RedisProject : Models.ProjectDataModel
+public class MongoServerProject : Models.ProjectDataModel
 {
     public string Host { get; set; } = string.Empty;
+    public int MaxSize { get; set; }
     public int Port { get; set; }
     public bool IsRunning { get; set; }
     public string ServerStatus { get; set; } = string.Empty;
