c4624022511bd5e507991376ce890674840c4193
Alex Giraldo <giraldojhong4@gmail.com>
2025-04-17 11:54:42 -0500

    fix

--- LIN.Types.Cloud.Edge/RequestModels/PostgresRequestManager.cs
+++ LIN.Types.Cloud.Edge/RequestModels/PostgresRequestManager.cs
@@ -2,7 +2,6 @@
 
 public class PostgresRequestModel
 {
-    public string Name { get; set; }
     public string DatabaseName { get; set; }
     public string DefaultUser { get; set; }
     public string Password { get; set; }
