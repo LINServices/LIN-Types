d51aad066b33d1333e8d69b5fd797ab0d7e70b32
Alex Giraldo <giraldojhong4@gmail.com>
2025-04-17 20:45:06 -0500

    Se arreglan errores

--- LIN.Types.Cloud.Edge/Models/BindingDataModel.cs
+++ LIN.Types.Cloud.Edge/Models/BindingDataModel.cs
@@ -5,5 +5,6 @@ public class BindingDataModel
     public int Id { get; set; }
     public int Port { get; set; }
     public string Name { get; set; } = string.Empty;
-
+    public ResourceDataModel Resource { get; set; } = null!;
+    public int ResourceId { get; set; }
 }
