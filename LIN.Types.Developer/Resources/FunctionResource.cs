0719adcc09bbe21b9b8abab06b147635883bdc2b
Alex Giraldo <giraldojhong4@gmail.com>
2025-04-19 13:23:21 -0500

    Nuevo manager de funciones, mejoras de codigo y optimización

---
+++ LIN.Types.Developer/Resources/FunctionResource.cs
@@ -0,0 +1,7 @@
+﻿namespace LIN.Types.Developer.Resources;
+
+public class FunctionResource : Models.ProjectDataModel
+{
+    public string GeneralPath { get; set; } = string.Empty;
+    public List<string> Endpoints { get; set; } = [];
+}
