b0b5d84a4f996a22cf65942e2d787868c0f0423c
Alex Giraldo <giraldojhong4@gmail.com>
2025-07-30 20:11:11 -0500

    mejoras

---
+++ LIN.Types.Payments/Request/PreferenceRequestModel.cs
@@ -0,0 +1,25 @@
+﻿using LIN.Types.Payments.Models;
+using System.Collections.Generic;
+
+namespace LIN.Types.Payments.Request;
+
+public class PreferenceRequestModel
+{
+    public string Document { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Webhook { get; set; } = string.Empty;
+    public CurrencyEnum Currency { get; set; } = CurrencyEnum.None;
+    public List<PaymentItemRequestModel> Items { get; set; } = [];
+}
+
+public enum CurrencyEnum
+{
+    None,
+    ARS,
+    BRL,
+    CLP,
+    MXN,
+    COP,
+    PEN,
+    UYU
+}
