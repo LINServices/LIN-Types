e6f65733398af69f12322a250f68a28bf21d152e
Alex Giraldo <giraldojhong4@gmail.com>
2025-09-21 11:10:28 -0500

    Correción de modelo.

--- LIN.Types.Communication/Models/DeviceOnAccountModel.cs
+++ LIN.Types.Communication/Models/DeviceOnAccountModel.cs
@@ -4,6 +4,8 @@ public class DeviceOnAccountModel
 {
     public string ConnectionId { get; set; } = string.Empty;
     public string Name { get; set; } = string.Empty;
-    public string OperativeSystem { get; set; } = string.Empty;
-    public string SurfaceFrom { get; set; } = string.Empty;
+
+    public Types.Enumerations.Platforms Platform { get; set; }
+    public Types.Enumerations.Browsers Browser { get; set; }
+    public Types.Enumerations.SurfaceFrom SurfaceFrom { get; set; }
 }
