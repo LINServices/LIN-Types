98cbe131734747fcf729bfefc41ccf6b7dc329ce
Alex Giraldo <giraldojhong4@gmail.com>
2025-10-12 10:52:45 -0500

    Mejoras

---
+++ LIN.Types.Communication/DTO/ReceiveCallDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace LIN.Types.Communication.DTO;
+
+public class ReceiveCallDTO
+{
+    public string RoomId { get; set; } = string.Empty;
+    public string Caller { get; set; } = string.Empty;
+}
