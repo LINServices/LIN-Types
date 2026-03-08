6e381509f78c1fd4e73860e28fd763bbdec3f826
Alex Giraldo <giraldojhong4@gmail.com>
2026-03-07 23:26:27 -0500

    Release model

---
+++ LIN.Types.Cloud.Edge/Models/ReleaseDataModel.cs
@@ -0,0 +1,12 @@
+﻿namespace LIN.Types.Cloud.Edge.Models;
+
+public class ReleaseDataModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Url { get; set; }
+    public DateTime CreatedAt { get; set; }
+
+    public int ContainerGroupId { get; set; }
+    public ContainerGroupDataModel ContainerGroup { get; set; } = null!;
+}
