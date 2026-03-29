b578024da5a7a1fd9fdda68f0a239052696fa183
Alex Giraldo <giraldojhong4@gmail.com>
2026-03-28 22:32:51 -0500

    Mejoras de tipos

--- LIN.Types.Cloud.Edge/Models/ServerDataModel.cs
+++ LIN.Types.Cloud.Edge/Models/ServerDataModel.cs
@@ -1,8 +1,43 @@
 ﻿namespace LIN.Types.Cloud.Edge.Models;
 
+public class RegionDataModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public List<ServerDataModel> Servers { get; set; } = [];
+}
+
 public class ServerDataModel
 {
     public int Id { get; set; }
+    public string Name { get; set; }
     public string HostIp { get; set; } = string.Empty;
     public string HostName { get; set; } = string.Empty;
+    public List<ServerFeature> ServerFeatures { get; set; } = [];
+    public int RegionId { get; set; }
+    public RegionDataModel Region { get; set; } = new RegionDataModel();
+}
+
+public class ServerFeature
+{
+    public string Feature { get; set; } = string.Empty;
+    public ServerDataModel ServerData { get; set; } = new ServerDataModel();
+    public int ServerDataId { get; set; }
+}
+
+public static class ServerConstants
+{
+    public const string FeatureNet = "dotnet";
+    public const string FeatureWeb = "web";
+    public const string FeatureNode = "node";
+    public const string FeatureMongo = "mongo";
+    public const string FeaturePostgres = "postgres";
+    public const string FeatureMySql = "mysql";
+    public const string FeatureRedis = "redis";
+    public const string FeatureValkey = "valkey";
+    public const string FeatureProxy = "proxy";
+    public const string FeatureRabbit = "rabbit";
+    public const string FeatureGateway = "gateway";
+    public const string FeaturePublish = "publish";
+    public const string FeatureMiniIo = "minio";
 }
---
+++ LIN.Types.Developer/Models/NotificationModel.cs
@@ -0,0 +1,13 @@
+﻿namespace LIN.Types.Developer.Models;
+
+public class NotificationModel
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+    public bool IsRead { get; set; } 
+    public DateTime CreatedAt { get; set; }
+    public DateTime? ReadAt { get; set; }
+    public ProfileDataModel Profile { get; set; } = default!;
+    public int ProfileId { get; set; }
+}
